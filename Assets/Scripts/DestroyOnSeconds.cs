c81063e348dd662d0dc24d49d3cc8d38de47d436
JAVHM <javanhmalpro@gmail.com>
2023-09-26 23:10:44 -0500

    fix

--- Assets/Scripts/DestroyOnSeconds.cs
+++ Assets/Scripts/DestroyOnSeconds.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class DestroyOnSeconds : MonoBehaviour
 {
-    private float destroyDelay = 4f;
+    public float destroyDelay = 30f;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, destroyDelay);
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        Destroy(gameObject);
     }
 }
